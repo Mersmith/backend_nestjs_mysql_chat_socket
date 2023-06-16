db906da6a570001799439c916ef17257c505eb50
Mersmith <mersmith14@gmail.com>
2023-06-16 15:26:03 -0500

    Paginatio Socket Rooms

--- src/chat/socket/chat.gateway.ts
+++ src/chat/socket/chat.gateway.ts
@@ -6,6 +6,7 @@ import { UserI } from 'src/user/model/user.inteface';
 import { UserService } from 'src/user/service/user-service/user.service';
 import { RoomService } from '../service/room-service/room.service';
 import { RoomI } from '../model/room.interface';
+import { PageI } from '../model/page.interface';
 
 @WebSocketGateway({
   cors: {
@@ -36,6 +37,8 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
         socket.data.user = user;
         const rooms = await this.roomService.getRoomsForUser(user.id, { page: 1, limit: 10 });
 
+        rooms.meta.currentPage = rooms.meta.currentPage - 1;
+
         return this.server.to(socket.id).emit('rooms', rooms);
         //this.title.push('Value ' + Math.random().toString());
         //this.server.emit('message', this.title);
@@ -52,6 +55,11 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     socket.disconnect();
   }
 
+  private disconnect(socket: Socket) {
+    socket.emit('Error', new UnauthorizedException());
+    socket.disconnect();
+  }
+
   @SubscribeMessage('createRoom')
   async onCreateRoom(socket: Socket, room: RoomI): Promise<RoomI> {
 
@@ -61,9 +69,15 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     return this.roomService.createRoom(room, socket.data.user);
   }
 
-  private disconnect(socket: Socket) {
-    socket.emit('Error', new UnauthorizedException());
-    socket.disconnect();
+  @SubscribeMessage('paginateRooms')
+  async onPaginateRoom(socket: Socket, page: PageI) {
+    page.limit = page.limit > 100 ? 100 : page.limit;
+
+    page.page = page.page + 1;
+    const rooms = await this.roomService.getRoomsForUser(socket.data.user.id, page);
+
+    rooms.meta.currentPage = rooms.meta.currentPage - 1;
+    return this.server.to(socket.id).emit('rooms', rooms);
   }
 
 }
